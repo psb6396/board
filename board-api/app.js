6872c72870bc26eabdc4fdbc01c3e0614f8dd57c
psb6396 <psb6396@naver.com>
2024-12-16 02:47:21 +0900

    s

--- board-api/app.js
+++ board-api/app.js
@@ -36,6 +36,24 @@ app.use(express.static(path.join(__dirname, 'uploads')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(cookieParser(process.env.COOKIE_SECRET)) //쿠키설정
+//세션설정
+app.use(
+   session({
+      resave: false, //세션 데이터가 변경되면 재저장할지 여부 -> 변경사항이 있어야 재저장
+      saveUninitialized: true, //초기화되지않은 세션 저장 여부 -> 초기화되지 않은 빈 세션도 저장
+      secret: process.env.COOKIE_SECRET, //세션 암호화 키
+      cookie: {
+         httpOnly: true, //javascript로 쿠키에 접근가능한지 여부 -> true일 경우 접근 X
+         secure: false, // https를 사용할때만 쿠키전송여부 -> http, https 둘다 사용가능
+      },
+   })
+)
+//passport 초기화, 세션연동
+app.use(passport.initialize()) //초기화
+app.use(passport.session()) //passport와 생성해둔 세션 연결
+
+
 //라우터 등록
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
--- board-api/routes/auth.js
+++ board-api/routes/auth.js
@@ -39,4 +39,65 @@ router.post('/join', async (req, res, next) => {
    }
 })
 
+//로그인 localhost:8000/auth/login
+router.post('/login', isNotLoggedIn, async (req, res, next) => {
+   passport.authenticate('local', (authError, user, info) => {
+      if (authError) {
+         //로그인 인증 중 에러 발생시
+         return res.status(500).json({ success: false, message: '인증 중 오류 발생', error: authError })
+      }
+      if (!user) {
+         //비밀번호 불일치 또는 사용자가 없을 경우 info.message를 사용해서 메세지 전달
+         return res.status(401).json({
+            success: false,
+            message: info.message || '로그인 실패',
+         })
+      }
+
+      // 인증이 정상적으로 되고 사용자를 로그인 상태로 바꿈
+      req.login(user, (loginError) => {
+         if (loginError) {
+            // 로그인 상태로 바꾸는 중 오류 발생시
+            return res.status(500).json({ success: false, message: '로그인 중 오류 발생', error: loginError })
+         }
+
+         //로그인 성공시
+         //status code를 주지 않으면 기본값은 200
+         res.json({
+            success: true,
+            message: '로그인 성공',
+            user: {
+               id: user.id,
+               nick: user.nick,
+            },
+         })
+      })
+   })(req, res, next)
+})
+
+//로그아웃 localhost:8000/auth/logout
+router.get('/logout', isLoggedIn, async (req, res, next) => {
+   // 사용자를 로그아웃 상태로 바꿈
+   req.logout((err) => {
+      if (err) {
+         //로그아웃 상태로 바꾸는중 에러가 났을 때
+         console.log(err)
+         return res.status(500).json({
+            success: false,
+            message: '로그아웃 중 오류가 발생했습니다.',
+            error: err,
+         })
+      }
+      //로그아웃 성공시 세션에 저장되어 있던 사용자 id를 삭제해주고 아래와 같은 결과를 response
+      //status code를 주지 않으면 기본값은 200
+      res.json({
+         success: true,
+         message: '로그아웃에 성공했습니다.',
+      })
+   })
+})
+
+//로그인 상태 확인 localhost:8000/auth/status
+router.get('/status', async (req, res, next) => {})
+
 module.exports = router
